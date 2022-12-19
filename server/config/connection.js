54335c8b8e9054dacf35c42f5c4beb0d3690576b
Hannah Folk <hfolk25@gmail.com>
2022-12-19 14:18:26 -0800

    set strictquery to false

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
+mongoose.set('strictQuery', false);
+
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/swhdecencyquiz', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
